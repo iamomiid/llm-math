694dfd0835f8c65402466a9f3d2e2555ba85be0f
Omid Seyfan <omid.seyfan@recharge.com>
2025-06-04 14:51:14 +0200

    Added improver

--- src/solver.ts
+++ src/solver.ts
@@ -7,14 +7,59 @@ import { readFile, writeFile } from "fs/promises";
 import path from "path";
 import { thinkAndExtract } from "./utils";
 
+const improveSystemPrompt = async (
+	model: LanguageModelV1,
+	systemPrompt: string,
+	result: MathResult,
+	steps: string
+) => {
+	const prompt = `You are going to improve the system prompt for solving math formulas.
+You are given a system prompt that was used to solve a formula using an LLM and the result of that formula.
+The LLM failed to solve this formula. Understand why it failed and improve the system prompt to make it more accurate.
+Do not include this formula in the improved system prompt.
+
+Initial System Prompt:
+"""
+${systemPrompt}
+"""
+
+Formula:
+"""
+${result.formula}
+"""
+
+Correct steps to solve the formula:
+"""
+${result.steps}
+"""
+
+Correct Result:
+"""
+${result.result}
+"""
+
+LLM Model thinking:
+"""
+${steps}
+"""
+	`;
+
+	const { object, thinking } = await thinkAndExtract(
+		model,
+		prompt,
+		z.object({
+			improvedSystemPrompt: z.string().describe("The improved system prompt."),
+		})
+	);
+
+	return object.improvedSystemPrompt;
+};
+
 export const solveResult = async (
 	model: LanguageModelV1,
+	systemPrompt: string,
 	result: MathResult
 ) => {
-	const systemPrompt = `
-You are a math expert. You are given a formula and you need to find the result of the formula.
-What is the result of this formula?
-`;
 	const userPrompt = `
 Formula: ${result.formula}
 `;
@@ -37,6 +82,26 @@ Formula: ${result.formula}
 	};
 };
 
+interface SolveResult {
+	correct: boolean;
+	steps: string;
+}
+
+interface ResultItem {
+	input: MathResult;
+	output: SolveResult;
+	improvedSystemPrompt: string;
+	improvedOutput: SolveResult;
+}
+
+interface Result {
+	spec: {
+		datasetId: string;
+		modelId: string;
+	};
+	results: Array<ResultItem>;
+}
+
 export const solveDataset = async () => {
 	const datasetId = "dataset-1749030125809";
 	const modelId = "gpt-4.1-nano";
@@ -52,16 +117,7 @@ export const solveDataset = async () => {
 		"results",
 		`solving-results-${Date.now()}.json`
 	);
-	const result: {
-		spec: {
-			datasetId: string;
-			modelId: string;
-		};
-		results: Array<{
-			input: MathResult;
-			output: { correct: boolean; steps: string };
-		}>;
-	} = {
+	const result: Result = {
 		spec: {
 			datasetId,
 			modelId,
@@ -69,13 +125,61 @@ export const solveDataset = async () => {
 		results: [],
 	};
 
+	const systemPrompt = `You are a math expert. You are given a formula and you need to find the result of the formula.
+What is the result of this formula?
+`;
+
+	let improvedSystemPrompt = systemPrompt;
+
+	let i = 0,
+		solved = 0,
+		improved = 0;
+
 	for (const input of datasetArray) {
-		const isSolved = await solveResult(model, input);
+		const isSolved = await solveResult(model, systemPrompt, input);
+		const isImprovedSolved = await solveResult(
+			model,
+			improvedSystemPrompt,
+			input
+		);
 
-		console.log("Solving formula", input.formula, isSolved.correct);
-		result.results.push({ input, output: isSolved });
+		const item: ResultItem = {
+			input,
+			output: isSolved,
+			improvedSystemPrompt,
+			improvedOutput: isImprovedSolved,
+		};
 
+		if (isImprovedSolved.correct) {
+			improved++;
+		} else {
+			console.log(
+				"Failed to solve formula by improved system prompt",
+				input.formula
+			);
+			improvedSystemPrompt = await improveSystemPrompt(
+				model,
+				systemPrompt,
+				input,
+				isSolved.steps
+			);
+		}
+
+		if (isSolved.correct) {
+			solved++;
+		}
+
+		if (!isSolved.correct && isImprovedSolved.correct) {
+			console.log("It worked! Improved system prompt:", improvedSystemPrompt);
+		}
+
+		result.results.push(item);
 		await writeFile(resultsPath, JSON.stringify(result, null, 2));
+		i++;
+
+		console.log("System Accuracy", (solved / i) * 100, "%");
+		console.log("Improved System Accuracy", (improved / i) * 100, "%");
+		console.log("--------------------------------");
 	}
 };
 
